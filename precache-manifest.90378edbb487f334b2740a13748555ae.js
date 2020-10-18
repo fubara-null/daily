5e95601f4f12ab5ca686a2aa855b1104982aed8f
Fubara Fortune <tammyfort50@gmail.com>
2020-10-18 16:18:11 +0100

    Updates

---
+++ precache-manifest.90378edbb487f334b2740a13748555ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2f5e409c60148d01d7a3b18d077608",
+    "url": "/daily/index.html"
+  },
+  {
+    "revision": "25fb86011e39ce5a5be8",
+    "url": "/daily/static/css/main.3c1e08e2.chunk.css"
+  },
+  {
+    "revision": "797cfb80083518f03aaa",
+    "url": "/daily/static/js/2.164569e7.chunk.js"
+  },
+  {
+    "revision": "30e75db0a2f3bf9213cb6380b68bc0bf",
+    "url": "/daily/static/js/2.164569e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25fb86011e39ce5a5be8",
+    "url": "/daily/static/js/main.65c88598.chunk.js"
+  },
+  {
+    "revision": "9b884d0f6ac059fb2484",
+    "url": "/daily/static/js/runtime-main.4b2822b4.js"
+  }
+]);
